7cd571184b3c7c906df4a8d0251a085243fe51f5
nandhinimano11@gmail.com <nandhinimano11@gmail.com>
2021-08-05 09:08:39 +0530

    forget password

---
+++ src/services/api/auth.ts
@@ -0,0 +1,44 @@
+import { common } from './utils/core';
+
+export const _forgotPasswordSendOtp = (payload: any, callback: any) => {
+  try {
+    let request = {
+      reqmethod: 'POST',
+      url: 'forgotPasswordSendOtp',
+      param: {},
+      reqdata: payload,
+    };
+    common(request)
+      .then((response: any) => {
+        response.status === 200 && callback(null, response.data);
+        // return response;
+      })
+      .catch((error) => {
+        console.log('inside error');
+        console.log('error', JSON.stringify(error));
+      });
+  } catch (e) {
+    callback(e.response.data, null);
+  }
+};
+export const _forgorPasswordVerifyOtp = (payload: any, callback: any) => {
+  try {
+    let request = {
+      reqmethod: 'POST',
+      url: 'forgorPasswordVerifyOtp',
+      param: {},
+      reqdata: payload,
+    };
+    common(request)
+      .then((response: any) => {
+        response.status == 200 && callback(null, response.data);
+        // return response;
+      })
+      .catch((error) => {
+        console.log('inside error');
+        console.log('error', JSON.stringify(error));
+      });
+  } catch (e) {
+    callback(e.response.data, null);
+  }
+};
