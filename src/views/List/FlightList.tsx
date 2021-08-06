7744702fe9719fda7bd632ac91f09fdfc3b43200
nandhinimano11@gmail.com <nandhinimano11@gmail.com>
2021-08-06 09:42:58 +0530

    merge

--- src/views/List/FlightList.tsx
+++ src/views/List/FlightList.tsx
@@ -31,7 +31,7 @@ import entertainment from "../../assets/Entertainment - Hotel@2x.png";
 import prizeAnalysis1 from "../../assets/Price Analysis - Illustration 1@2x.png";
 import prizeAnalysis2 from "../../assets/Price Analysis - Illustration 2@2x.png";
 import user from "../../assets/Icon feather-user@2x.png";
-import { Button, InputAdornment, Typography } from "@material-ui/core";
+import { Button, InputAdornment, Popover, Typography } from "@material-ui/core";
 import TrackPricesContainer from "../TrackPrices/index";
 import Box from "@material-ui/core/Box";
 import { Divider } from "@material-ui/core";
@@ -41,7 +41,20 @@ import SpiceJet from "../../assets/Flight logo - 3@2x.png";
 import indigo from "../../assets/Flight logo - 2@2x.png";
 import flightIcon from "../../assets/Icon material-flight@2x.png";
 // import BottomGrid from '../Airvays info/index'
-import { _searchFlights } from '../../services/api/flight'
+import { _searchFlights } from '../../services/api/flight';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
+import Popper, { PopperPlacementType } from '@material-ui/core/Popper';
+import Fade from '@material-ui/core/Fade';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import _ from 'lodash';
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -119,10 +132,34 @@ const Amenities = [
 export default function HotelsList() {
   const classes = useStyles();
   const [value, setValue] = React.useState("One-way");
+  const [filtersData, setFiltersData] = React.useState([]);
   const [selectedDate, setSelectedDate] = React.useState<Date | null>(
     new Date("2014-08-18T21:11:54")
   );
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [open, setOpen] = React.useState(false);
+  const [placement, setPlacement] = React.useState<PopperPlacementType>();
+  const handleClick = (newPlacement: PopperPlacementType) => (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    setAnchorEl(event.currentTarget);
+    setOpen((prev) => placement !== newPlacement || !prev);
+    setPlacement(newPlacement);
+  };
+  const [checked, setChecked] = React.useState([0]);
 
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
   const handleDateChange = (date: Date | null) => {
     setSelectedDate(date);
   };
@@ -135,24 +172,21 @@ export default function HotelsList() {
   const searchFlights = () => {
     _searchFlights({
       "from": "MAA",
-      "to": "DEL",
+      "to": "LON",
       "currencyCode": "INR",
       "type": "one-way",
-      "from_date": "2021-07-29",
-      "to_date": "2021-07-31",
+      "from_date": "2021-08-21",
+      "to_date": "2021-08-31",
       "no_of_people": {
         "adults": 1,
         "children": 0,
         "infants": 0
       },
-      "class": "ECONOMY",
-      "filter": "fastest",
-      "price_range_from": "",
-      "price_range_to": ""
+      "class": "ECONOMY"
     }, function (error: any, response: any) {
       if (error == null) {
         if (response.status == 200) {
-
+          setFiltersData(response.result)
         } else {
         }
       } else if (response == null) {
@@ -160,6 +194,14 @@ export default function HotelsList() {
       }
     });
   };
+  console.log(filtersData, "response22")
+ 
+
+ 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={classes.root}>
       <Grid container spacing={3} className={classes.flightTop}>
@@ -504,7 +546,9 @@ export default function HotelsList() {
             </Grid>
           </Grid>
 
+
           {/* serach results */}
+          <div  style ={{background:'#E4F4FC'}}>
           <Grid container>
             <Grid container spacing={3} style={{ marginTop: "20px" }}>
               <Grid item xs={12}>
@@ -526,15 +570,52 @@ export default function HotelsList() {
 
             <Grid container spacing={3} style={{ marginTop: "20px" }}>
               <Grid item xs={10} style={{ display: "flex" }}>
-                <Button
+              <ClickAwayListener onClickAway={()=>setOpen(false)}>
+              <Button
                   style={{
                     color: "#FFF",
                     background: "#4BAFC9",
                     borderRadius: "20px",
                   }}
+                  onClick={handleClick('bottom')}
                 >
                   Airlines : All
                 </Button>
+                </ClickAwayListener>
+              
+                  <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+                    {({ TransitionProps }) => (
+                      <Fade {...TransitionProps} timeout={350}>
+                        <Paper>
+                          <List >
+                            {[0, 1, 2, 3].map((value) => {
+                              const labelId = `checkbox-list-label-${value}`;
+                              return (
+                                <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
+                                  <ListItemIcon>
+                                    <Checkbox
+                                      edge="start"
+                                      checked={checked.indexOf(value) !== -1}
+                                      tabIndex={-1}
+                                      disableRipple
+                                      inputProps={{ 'aria-labelledby': labelId }}
+                                    />
+                                  </ListItemIcon>
+                                  <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                                  <ListItemSecondaryAction>
+                                    <IconButton edge="end" aria-label="comments">
+                                      <CommentIcon />
+                                    </IconButton>
+                                  </ListItemSecondaryAction>
+                                </ListItem>
+                              );
+                            })}
+                          </List>
+                        </Paper>
+                      </Fade>
+                    )}
+                  </Popper>
+                
                 <Button
                   style={{
                     color: "#FFF",
@@ -589,112 +670,118 @@ export default function HotelsList() {
                 </div>
               </Grid>
             </Grid>
+            {filtersData.map((x: any) => (
+              <Grid
+                container
+                style={{
+                  display: "flex",
+                  marginTop: "40px",
+                  backgroundColor: "white",
+                  padding: "10px",
+                }}
+              >
+                {/* <Grid container> */}
 
-            {Array.from({ length: 10 }, (x: any, i) => (
-              <div>
+                {x.itineraries[0].segments.map((item: any) => (
+                  <>
+                    {/* <Grid item xs={2}>
+                 
+                    </Grid> */}
+                    <Grid container item xs={10} style={{ color: "#1C2460", marginTop: '15px', display: 'flex', justifyContent: 'space-between' }}>
+                      <div>
+                        <div>
+                          <img
+                            style={{ marginLeft: "30px" }}
+                            src={SpiceJet}
+                          ></img>
+                        </div>
+                        <Typography
+                          style={{
+                            fontSize: "14px",
+                            color: "#1C2460",
+                            opacity: "40%",
+                            marginLeft: "35px",
+                          }}
+                        >
+                          SpiceJet
+                        </Typography>
+                      </div>
+
+                      <div>
+                        {item.departure.at}
+                        <br />
+                        <Typography style={{ marginTop: '5px' }}>Chennai</Typography>
+                        <br />
+                        {item.departure.iataCode}
+                      </div>
+                      <div>
+                        <Typography style={{ textAlign: 'center' }}>Direct</Typography>
+                        <div style={{ display: "flex" }}>
+                          {"-------------------------"}
+                          <img src={flightIcon}></img>
+                          {"-------------------------"}
+                        </div>
+                        <Typography style={{ marginTop: '5px', textAlign: 'center' }}>{x.itineraries[0].duration}</Typography>
+                      </div>
+                      <div>
+                        {item.arrival.at}
+                        <Typography style={{ marginTop: '5px' }}>  Bengaluru Intl</Typography>
+                        <br />
+                        {item.arrival.iataCode}
+                      </div>
+
+                    </Grid>
+
+                  </>
+                ))}
+
+
                 <Grid
-                  container
+                  item
+                  xs={2}
                   style={{
+                    alignItems: "center",
+                    justifyContent: "center",
                     display: "flex",
-                    marginTop: "40px",
-                    backgroundColor: "white",
-                    padding: "10px",
+                    borderLeft: "1px solid #EDEDED",
                   }}
                 >
-                  <Grid container>
-                    <Grid item xs={2}>
-                      <div>
-                        <img
-                          style={{ marginLeft: "30px" }}
-                          src={SpiceJet}
-                        ></img>
-                      </div>
-                      <Typography
+                  <div>
+                    <Typography>
+                      <span
                         style={{
-                          fontSize: "14px",
+                          fontSize: "22px",
+                          fontWeight: 500,
                           color: "#1C2460",
-                          opacity: "40%",
-                          marginLeft: "35px",
                         }}
                       >
-                        SpiceJet
-                      </Typography>
-                    </Grid>
-
-                    <Grid item xs={1} style={{ color: "#1C2460" }}>
-                      <div>
-                        <p>09:05</p>
-                        <p>
-                          Chennai
-                          <br />
-                          MAA
-                        </p>
-                      </div>
-                    </Grid>
-                    <Grid
-                      item
-                      xs={4}
-                      style={{
-                        alignItems: "center",
-                        textAlign: "center",
-                        justifyContent: "center",
-                      }}
+                        {x.price.currency}{x.price.base}
+                      </span>
+                    </Typography>
+                    <br />
+                    <Button
+                      variant="contained"
+                      style={{ background: "#DCAB5E", color: "#fff" }}
                     >
-                      <Typography>Direct</Typography>
-                      <div style={{ display: "flex" }}>
-                        {"-------------------------"}
-                        <img src={flightIcon}></img>
-                        {"-------------------------"}
-                      </div>
-                      <Typography>0 hr 40 mins</Typography>
-                    </Grid>
-                    <Grid item xs={2} style={{ color: "#1C2460" }}>
-                      <div>
-                        <p>09:45</p>
-                        <p>
-                          Bengaluru Intl
-                          <br />
-                          BLR
-                        </p>
-                      </div>
-                    </Grid>
-                    <Grid item xs={1}></Grid>
-                    {/* <div style={{ display: "flex", marginTop: "20px" }}></div> */}
-                    {/* </Grid> */}
-                    <Grid
-                      item
-                      xs={2}
-                      style={{
-                        alignItems: "center",
-                        justifyContent: "center",
-                        display: "flex",
-                        borderLeft: "1px solid #EDEDED",
-                      }}
-                    >
-                      <div>
-                        <Typography>
-                          <span
-                            style={{
-                              fontSize: "22px",
-                              fontWeight: 500,
-                              color: "#1C2460",
-                            }}
-                          >
-                            $120{" "}
-                          </span>
-                        </Typography>
-                        <br />
-                        <Button
-                          variant="contained"
-                          style={{ background: "#DCAB5E", color: "#fff" }}
-                        >
-                          View Details
-                        </Button>
-                      </div>
-                    </Grid>
-                  </Grid>
+                      View Details
+                    </Button>
+                  </div>
                 </Grid>
+                {/* </Grid> */}
+              </Grid>
+            ))}
 
+
+
+
+
+
+
+
+
+
+            {Array.from({ length: 3 }, (x: any, i) => (
+              <div>
                 <Grid
                   container
                   style={{
@@ -855,6 +942,8 @@ export default function HotelsList() {
               {" "}
             </Grid>
           </Grid>
+          </div>
+    
           {/* <BottomGrid /> */}
         </Grid>
       </Grid>
